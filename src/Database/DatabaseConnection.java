7ee8cb6afd53f4fa4a73300e455e220395338182
Fadil558 <rujaklah@gmail.com>
2025-11-02 18:46:44 +0800

    Menyiapkan package MVC dan Pengaturan Database

---
+++ src/Database/DatabaseConnection.java
@@ -0,0 +1,16 @@
+// lib sqlite-jdbc: https://github.com/xerial/sqlite-jdbc/
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+
+    private static final String URL = "jdbc:sqlite:contacts.db";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+}
